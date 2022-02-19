a76fbd7ecea548a35f2338ab7191b1c941e4bd97
StarWars1980 <iafe2800@colorado.edu>
2022-02-18 19:48:24 -0700

    last4

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -1,1 +1,28 @@
 #include "BinaryTree.hpp"
+
+    PersonTree::PersonTree(){
+
+    }
+    PersonTree::~PersonTree(){
+
+    }
+    void PersonTree::searchTree(string) // searching tree by name (find you in tree)
+    {
+
+    }
+    void PersonTree::addtoPersonTree(string, string, vector<string>, vector<string>) // adding node to tree
+    {
+
+    }
+    void PersonTree::similarNode(string, string, vector<string>, vector<string>) // finding node similar to you
+    {
+
+    }
+    void PersonTree::printSimilarNode(string, string, vector<string>, vector<string>) // printing node info
+    {
+
+    }
+    void PersonTree::destoryNode(PersonNode*)
+    {
+
+    }
